07cd78e97354f9669b74b19942aff460247e86ec
Simon Maribo <simonmaribo@outlook.dk>
2022-08-31 21:40:29 +0200

    Small changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,9 @@ export default function Header(){
     return (
         <div className="flex flex-col-reverse gap-10 justify-between items-center md:flex-row">
             <div className="flex flex-col gap-4 mb-5">
-                <h1 className="text-grey-800 text-[30px] font-bold">Simon Maribo</h1>
+                <h1 className="text-grey-900 text-[30px] font-bold">Simon Maribo</h1>
                 <p className="text-grey-800">
                     Hej, mit navn er Simon, jeg er 16 år gammel og har kodet i diverse sprog siden jeg var 12. Hvis du har en ide til et projekt, så ikke tøv med at kontakte mig.</p>
-                <div className="flex flex-row gap-2 leading-none text-grey-800 font-normal">
-                    <MdLocationOn/>
-                    <p className="text-grey-800">Danmark, Hillerød</p>
-                </div>
                 <div className="flex flex-row gap-3">
                     <SocialIcon icon={<AiFillGithub size={25}/>} href="https://github.com/simonmaribo"/>
                     <SocialIcon icon={<FaDiscord size={25}/>} href="https://discord.com/users/261195644314714113"/>
@@ -23,7 +19,7 @@ export default function Header(){
                     <SocialIcon icon={<FaEnvelope size={25}/>} href="mailto:simonmaribo@outlook.dk"/>
                 </div>
             </div>
-            <img className="rounded-full flex-grow max-h-[224px]" src="./mig.png" alt="Billede af Simon Maribo"/>
+            <img className="rounded-full flex-grow max-h-[224px] outline outline-offset-4 outline-3 outline-[#0050ff]" src="./mig.png" alt="Billede af Simon Maribo"/>
         </div>
     )
 }
--- components/Section.tsx
+++ components/Section.tsx
@@ -1,7 +1,7 @@
 export default function Section({ children, title, subtitle }: { children: React.ReactNode, title: string, subtitle: string }) {
   return (
     <div>
-        <h2 className="text-grey-800 text-[32px] font-bold">{title}</h2>
+        <h2 className="text-grey-900 text-[20px] font-bold">{title}</h2>
         <p className="text-grey-800">{subtitle}</p>
         {children}
     </div>
